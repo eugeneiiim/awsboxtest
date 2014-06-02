3f58403dd7d29cb74665373df35cba6ff4e2a92b
Eugene Marinelli <eugeneiiim@gmail.com>
2014-06-01 22:10:03 -0700

    oops

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function(req, res) {
+  res.send('Hi max');
+});
+
 var port = process.env.PORT || 5000;
 console.log('web.js listening on port %s.', port);
 app.listen(port);
